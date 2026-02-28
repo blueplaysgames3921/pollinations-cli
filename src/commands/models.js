7ba1bea5a5cec06afabeb269664577b1e03586c3
BLUEGAMINGGM <123081329+blueplaysgames3921@users.noreply.github.com>
2026-02-28 23:55:49 +0530

    Create models.js

---
+++ src/commands/models.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import chalk from 'chalk';
+
+export async function listModels() {
+  try {
+    const { data } = await axios.get('https://gen.pollinations.ai/openai/models');
+    console.log(chalk.bold.green('\nAvailable Models:'));
+    data.data.forEach(m => console.log(`- ${m.id}`));
+  } catch (err) {
+    console.log(chalk.red('Could not fetch models.'));
+  }
+}
