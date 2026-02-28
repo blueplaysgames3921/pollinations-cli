fa628507d7dbadcf4b0ee01d820339201bd597fa
BLUEGAMINGGM <123081329+blueplaysgames3921@users.noreply.github.com>
2026-03-01 00:41:27 +0530

    Update config-store.js

--- src/lib/config-store.js
+++ src/lib/config-store.js
@@ -1,2 +1,8 @@
 import Conf from 'conf';
-export const config = new Conf({ projectName: 'pollinations' });
+import path from 'path';
+import os from 'os';
+
+export const config = new Conf({
+  projectName: 'pollinations',
+  cwd: path.join(os.homedir(), '.pollinations')
+});
