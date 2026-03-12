846b5cc00be3413385bd54abf69ef4904855d3cc
BLUEGAMINGGM <123081329+blueplaysgames3921@users.noreply.github.com>
2026-03-12 17:58:10 +0530

    Update pollinations.js

--- bin/pollinations.js
+++ bin/pollinations.js
@@ -16,7 +16,7 @@ import { templateSave, templateRun } from '../src/commands/template.js';
 import { config } from '../src/lib/config-store.js';
 import chalk from 'chalk';
 
-program.name('pollinations').version('1.2.2');
+program.name('pollinations').version('1.2.3');
 
 program.command('login')
   .description('Authenticate and initialize the Pollinations dashboard')
